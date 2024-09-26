3c7bf054d5898318d6a87e5f3f3eaff992247725
smoklien <localnext07@gmail.com>
2024-09-26 14:46:34 +0300

    small changes

--- node/app.js
+++ node/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware для обробки JSON-запитів
+app.use(express.json());
+
 // Імпорт роутера для повідомлень
 const messagerRouter = require('./routes/messager');
 app.use('/api/messager', messagerRouter);
-
-
-// Middleware для обробки JSON-запитів
-app.use(express.json());
 
 // Базовий роут для перевірки роботи сервера
 app.get('/', (req, res) => {
