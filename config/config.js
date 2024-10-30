f895d94709c07b7abf620b4539d64b74f31f6f68
Smoklien <localnext07@gmail.com>
2024-10-30 11:07:49 +0000

    Minor change

--- config/config.js
+++ config/config.js
@@ -5,8 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 module.exports = {
-    PORT: process.env.PORT || 5000,
-    FRONT_URL: process.env.FRONT_URL || `http://localhost:3000/`,
+    PORT: process.env.PORT || 3000,
     GROQ_API_KEY: process.env.GROQ_API_KEY,
     DEFAULT_MODEL: process.env.DEFAULT_MODEL,
     MONGOOSE_URL: process.env.MONGOOSE_URL,
